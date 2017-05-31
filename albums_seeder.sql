6fc2019729632e2ec1b3e53eb0520fdb736656d3
Mike Festger <festger@gmail.com>
2017-05-31 12:01:50 -0500

    albums_seeder.sql
    ADDED:
    
      - Completed exercise by demonstrating how to insert data into an established data structure. Data pulled from Wikipedia per instructions.

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '46.3', 'Pop, Rock, R&B'),
+  ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard Rock'),
+  ('Pink Floyd', 'Dark Side of the Moon', '1973', '24.2', 'Progressive Rock'),
+  ('Whitney Houston / Various Artists', 'The Bodyguard', '1992', '27.4', 'R&B, Soul, Pop, Soundtrack');
