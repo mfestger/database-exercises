eb1c8c23dbbcea195a9a4c81b76255c3637b0b6d
Mike Festger <festger@gmail.com>
2017-05-31 10:59:58 -0500

    tables-lesson.sql
    ADDED:
    
      - Added table per exercise instruction
    
     TO BE ADDED:
    
      - Determine correct syntax for adding a date to the table

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE  albums (
+  id INT UNSIGNED NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  artist VARCHAR(255),
+  name VARCHAR(255),
+  release_date DOUBLE,
+  sales DOUBLE,
+  genre VARCHAR(255)
+);
